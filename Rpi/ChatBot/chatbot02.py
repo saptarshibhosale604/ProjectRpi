8e73e5ac231d823708001061e6c588da5f3b48ba
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-09 15:13:19 +0530

    Update chatbot02.py

--- Rpi/ChatBot/chatbot02.py
+++ Rpi/ChatBot/chatbot02.py
@@ -4,10 +4,10 @@ import os
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
-# myTestKey05
+# myTestKey06
 
 if not os.environ.get("OPENAI_API_KEY"):
-  os.environ["OPENAI_API_KEY"] = "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"
+  os.environ["OPENAI_API_KEY"] = "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"
 
 # ~ model = ChatOpenAI(model="gpt-4o-mini")
 model = ChatOpenAI(model="gpt-3.5-turbo")
