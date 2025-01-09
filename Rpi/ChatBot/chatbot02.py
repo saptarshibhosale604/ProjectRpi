17b38a9d490d181d4601a4ceaab045b060daaa85
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-09 15:08:34 +0530

    Update chatbot02.py

--- Rpi/ChatBot/chatbot02.py
+++ Rpi/ChatBot/chatbot02.py
@@ -1,5 +1,4 @@
-
-import getpass
+# import getpass
 import os
 # from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_openai import ChatOpenAI
@@ -10,18 +9,18 @@ from langchain_core.messages import HumanMessage, SystemMessage
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = "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"
 
-# # ~ model = ChatOpenAI(model="gpt-4o-mini")
-# model = ChatOpenAI(model="gpt-3.5-turbo")
+# ~ model = ChatOpenAI(model="gpt-4o-mini")
+model = ChatOpenAI(model="gpt-3.5-turbo")
 
-# messages = [
-#     SystemMessage("Translate the following from English into German"),
-#     HumanMessage("hi!"),
-# ]
+messages = [
+    SystemMessage("Translate the following from English into German"),
+    HumanMessage("hi!"),
+]
 
 # Invoke the model and store the response
-# response = model.invoke(messages)
+response = model.invoke(messages)
 # llm = ChatOpenAI()
-response = ChatOpenAI().invoke("Here is a fun fact about Pluto:")
+# response = ChatOpenAI().invoke("Here is a fun fact about Pluto:")
 
 # Print the response
 print(response)
