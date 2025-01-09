29d1da2807ef7ef9bb42073dc2bd3f7a655fc620
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-09 17:36:17 +0530

    Update 02_chatbot.py

--- Langchain/02_chatbot.py
+++ Langchain/02_chatbot.py
@@ -1,1 +1,42 @@
 
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+# from langchain.schema import (
+#     AIMessage,
+#     HumanMessage,
+#     SystemMessage
+# )
+
+# myTestKey07 #saptarshibhosale604@gmail.com
+f = open('openai_api_key.txt')
+# print("openai_api_key: ", f.read())
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = f.read().strip() 
+
+# Method 01
+# model = ChatOpenAI(model="gpt-4o-mini")
+# model = ChatOpenAI(model="gpt-3.5-turbo")
+chat = ChatOpenAI()
+
+result = chat([HumanMessage(content="Can you tell me a fact about Earth?")])
+
+# messages = [
+#     SystemMessage("Translate the following from English into German"),
+#     HumanMessage("hi!"),
+# ]
+
+# Invoke the model and store the response
+# response = model.invoke(messages)
+
+# Print the response
+print(result.content)
+
+# Method 02
+# llm = ChatOpenAI()
+# response = llm.invoke("Here is a fun fact about Pluto:")
+
+# # Print the response
+# print(response)
+
