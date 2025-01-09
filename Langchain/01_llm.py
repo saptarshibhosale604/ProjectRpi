60874d27f26db40ab178b4cce2a266ab51eaa6e0
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-09 17:28:07 +0530

    Update 01_llm.py

--- Langchain/01_llm.py
+++ Langchain/01_llm.py
@@ -1,19 +1,16 @@
-# import getpass
 import os
-# from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
-# myTestKey07
+# myTestKey07 #saptarshibhosale604@gmail.com
 f = open('openai_api_key.txt')
-# api_key = f.read()
-
 # print("openai_api_key: ", f.read())
 
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = f.read().strip() 
 
-# ~ model = ChatOpenAI(model="gpt-4o-mini")
+# # Method 01
+# # model = ChatOpenAI(model="gpt-4o-mini")
 # model = ChatOpenAI(model="gpt-3.5-turbo")
 
 # messages = [
@@ -21,19 +18,15 @@ if not os.environ.get("OPENAI_API_KEY"):
 #     HumanMessage("hi!"),
 # ]
 
-# Invoke the model and store the response
+# # Invoke the model and store the response
 # response = model.invoke(messages)
 
-# response = model.invoke(messages)
+# # Print the response
+# print(response)
 
+# Method 02
 llm = ChatOpenAI()
 response = llm.invoke("Here is a fun fact about Pluto:")
-# response = llm.invoke.generate(["Here is a fun fact about Pluto:", "Here is a fun fact about mars:"])
 
 # Print the response
 print(response)
-
-# llm = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"])
-
-# print(llm('Here is a fun fact about Pluto:'))
-
