a009a1f6f293e58ee6124d757640947ac28a13ba
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-10 11:42:55 +0530

    Create 04_documentLoader.py

---
+++ Langchain/04_documentLoader.py
@@ -0,0 +1,15 @@
+from langchain.document_loaders import CSVLoader
+
+loader = CSVLoader('/penguins.csv')
+
+data = loader.load()
+
+# print(data)
+
+print("type:",type(data))
+
+print("data0:",data[0])
+
+print("data1:",data[1])
+
+print("data0content:",data[0].page_content)
