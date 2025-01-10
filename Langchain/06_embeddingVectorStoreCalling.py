203abfcf9b744c36ea10dee91daa129aed0acf0a
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-10 16:02:56 +0530

    Update 06_embeddingVectorStoreCalling.py

--- Langchain/06_embeddingVectorStoreCalling.py
+++ Langchain/06_embeddingVectorStoreCalling.py
@@ -24,62 +24,37 @@ def us_constitution_helper(question):
     
     Follow the steps below to fill out this function:
     '''
-    # # PART ONE:
-    # # LOAD "some_data/US_Constitution in a Document object
-    # loader = TextLoader("./US_Constitution.txt")
-    # documents = loader.load()
-    # print("\n\n## loader ##:",loader)
-    # print("\n\n## len(documents) ##:",len(documents))
     
-    # # PART TWO
-    # # Split the document into chunks (you choose how and what size)
-    # text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=500)
-    # docs = text_splitter.split_documents(documents)
-    # print("\n\n## text_splitter ##:",text_splitter)
-    # print("\n\n## len(docs) ##:",len(docs))
-    # print("\n\n## len(docs[0].page_content) ##:",len(docs[0].page_content))
-    # print("\n\n## docs[0] ##:",docs[0])
-    
-    # # PART THREE
-    # # EMBED THE Documents (now in chunks) to a persisted ChromaDB
-    # embedding_function = OpenAIEmbeddings()
-    # db = Chroma.from_documents(docs, embedding_function,persist_directory='./US_Constitution')
-    # # db.persist() # old method
-    # print("\n\n## embedding_function ##:",embedding_function)
-    # print("\n\n## db ##:",db)
-
     # PART FOUR
     # return most relevent documents 
-    embedding_function = OpenAIEmbeddings()    
-    db_connection = Chroma(persist_directory='./US_Constitution',embedding_function=embedding_function)
-    print("\n\n## embedding_function ##:",embedding_function)
-    print("\n\n## db_connection ##:",db_connection)
+    # embedding_function = OpenAIEmbeddings()    
+    # db_connection = Chroma(persist_directory='./US_Constitution',embedding_function=embedding_function)
+    # print("\n\n## embedding_function ##:",embedding_function)
+    # print("\n\n## db_connection ##:",db_connection)
     
-    retriever = db_connection.as_retriever()
-    # search_kwargs = {"score_threshold":0.8,"k":4}
-    # docs = retriever.get_relevant_documents(question, search_kwargs=search_kwargs)
+    # retriever = db_connection.as_retriever()
+    # # search_kwargs = {"score_threshold":0.8,"k":4}
+    # # docs = retriever.get_relevant_documents(question, search_kwargs=search_kwargs)
+    # # docs = retriever.get_relevant_documents(question)
     # docs = retriever.get_relevant_documents(question)
-    docs = retriever.get_relevant_documents(question)
 
-    print("\n\n## len(docs) ##:",len(docs))
-    print("\n\n## docs[0].page_content ##:",docs[0].page_content)
+    # print("\n\n## len(docs) ##:",len(docs))
+    # print("\n\n## docs[0].page_content ##:",docs[0].page_content)
     
     # PART FIVE
     # Use ChatOpenAI and ContextualCompressionRetriever to return the most
     # relevant part of the documents.
 
-    # results = db.similarity_search("What is the 13th Amendment?")
-    # print(results[0].page_content) # NEED TO COMPRESS THESE RESULTS!
-    # llm = ChatOpenAI(temperature=0)
-    # compressor = LLMChainExtractor.from_llm(llm)
-    # embedding_function = OpenAIEmbeddings()    
-    # db_connection = Chroma(persist_directory='./US_Constitution',embedding_function=embedding_function)
+    llm = ChatOpenAI(temperature=0)
+    compressor = LLMChainExtractor.from_llm(llm)
+    embedding_function = OpenAIEmbeddings()    
+    db_connection = Chroma(persist_directory='./US_Constitution',embedding_function=embedding_function)
 
-    # compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, 
-    #                                                        base_retriever=db_connection.as_retriever())
+    compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, 
+                                                           base_retriever=db_connection.as_retriever())
 
-    # compressed_docs = compression_retriever.get_relevant_documents(question)
+    compressed_docs = compression_retriever.get_relevant_documents(question)
 
-    # print(compressed_docs[0].page_content)
+    print(compressed_docs[0].page_content)
     
 us_constitution_helper("What was the 1st amendment?")
