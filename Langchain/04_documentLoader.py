658a679403df4f814e75375d40469da83d7e65d2
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-10 13:13:37 +0530

    Update 04_documentLoader.py

--- Langchain/04_documentLoader.py
+++ Langchain/04_documentLoader.py
@@ -37,34 +37,41 @@ from langchain.prompts import (
 )
 from langchain_community.document_loaders import WikipediaLoader
 
-def answer_question_about(person_name,question):
-    # Get Wikipedia Article
-    docs = WikipediaLoader(query=person_name,load_max_docs=1)
-    context_text = docs.load()[0].page_content
+docs = WikipediaLoader(query="god",load_max_docs=1)
 
-    print("\n\n## docs ##:",docs)
-    print("\n\n## context_text ##:",context_text)
-    
-    # Connect to OpenAI Model
-    # myTestKey07 #saptarshibhosale604@gmail.com
-    f = open('openai_api_key.txt')
-    api_key = f.read().strip()
-    model = ChatOpenAI(openai_api_key=api_key)
-    
-    print("\n\n## model ##:",model)
-    
-    # Ask Model Question
-    human_prompt = HumanMessagePromptTemplate.from_template('Answer this question\n{question}, here is some extra context:\n{document}')
-    print("\n\n## human_prompt ##:",human_prompt)
-    
-    # Assemble chat prompt
-    chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
-    print("\n\n## chat_prompt ##:",chat_prompt)
-    
-    #result
-    result = model.invoke(chat_prompt.format_prompt(question=question,document=context_text).to_messages())
-    print("\n\n## docs ##:",docs)
-    
-    print(result.content)
+print("\n\n## docs ##:",docs)
+print("\n\n## docs.load() ##:",docs.load())
+print("\n\n## docs.load()[0].page_content ##:",docs.load()[0].page_content)
 
-answer_question_about("Claude Shannon","When was he born?")
+
+# def answer_question_about(person_name,question):
+#     # Get Wikipedia Article
+#     docs = WikipediaLoader(query=person_name,load_max_docs=1)
+#     context_text = docs.load()[0].page_content
+
+#     print("\n\n## docs ##:",docs)
+#     print("\n\n## context_text ##:",context_text)
+    
+#     # Connect to OpenAI Model
+#     # myTestKey07 #saptarshibhosale604@gmail.com
+#     f = open('openai_api_key.txt')
+#     api_key = f.read().strip()
+#     model = ChatOpenAI(openai_api_key=api_key)
+    
+#     print("\n\n## model ##:",model)
+    
+#     # Ask Model Question
+#     human_prompt = HumanMessagePromptTemplate.from_template('Answer this question\n{question}, here is some extra context:\n{document}')
+#     print("\n\n## human_prompt ##:",human_prompt)
+    
+#     # Assemble chat prompt
+#     chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
+#     print("\n\n## chat_prompt ##:",chat_prompt)
+    
+#     #result
+#     result = model.invoke(chat_prompt.format_prompt(question=question,document=context_text).to_messages())
+#     print("\n\n## docs ##:",docs)
+    
+#     print(result.content)
+
+# answer_question_about("Claude Shannon","When was he born?")
