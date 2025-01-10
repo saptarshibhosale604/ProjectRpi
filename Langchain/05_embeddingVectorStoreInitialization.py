560538c87254c6cda8ba2072b487885e825bd1ba
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-10 15:29:02 +0530

    Update 05_embeddingVectorStoreInitialization.py

--- Langchain/05_embeddingVectorStoreInitialization.py
+++ Langchain/05_embeddingVectorStoreInitialization.py
@@ -13,8 +13,8 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_community.retrievers import ContextualCompressionRetriever
-from langchain_community.retrievers.document_compressors import LLMChainExtractor 
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor 
 
 import os
 
