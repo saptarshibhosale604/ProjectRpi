913e312d992d7a60ac27f59c2eb676c83e594c85
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-11 10:33:22 +0530

    Update 10_conversationBufferMemory.py

--- Langchain/10_conversationBufferMemory.py
+++ Langchain/10_conversationBufferMemory.py
@@ -14,49 +14,37 @@ if not os.environ.get("OPENAI_API_KEY"):
 
 llm = ChatOpenAI(temperature=0.0)
 
-## part 01, create a conversation memory
-memory = ConversationBufferMemory()
+# ## part 01, create a conversation memory
+# memory = ConversationBufferMemory()
+# conversation = ConversationChain(
+#     llm=llm, 
+#     memory = memory,
+#     verbose=True
+# )
+# conversation.predict(input="Hello, nice to meet you!")
+# conversation.predict(input="Tell me about the Einstein-Szilard Letter in 2 lines ")    
+# print("\n\n## memory.buffer ##:",memory.buffer)
+# print("\n\n## memory.load_memory_variables({}) ##:",memory.load_memory_variables({}))
+# memory.save_context({"input": "Very Interesting."}, 
+#                     {"output": "Yes, it was my pleasure as an AI to answer."})
+# print("\n\n## memory.load_memory_variables({}) ##:",memory.load_memory_variables({}))
+# conversation.memory
 
-conversation = ConversationChain(
+# ## part 02, save the conversation memory
+# pickled_str = pickle.dumps(conversation.memory)
+# print("\n\n## pickled_str ##:", pickled_str)
+# with open('memory.pkl','wb') as f:
+#     f.write(pickled_str)
+
+## part 03, load the conversation memory
+new_memory_load = open('memory.pkl','rb').read()
+
+llm = ChatOpenAI(temperature=0.0)
+reload_conversation = ConversationChain(
     llm=llm, 
-    memory = memory,
+    memory = pickle.loads(new_memory_load),
     verbose=True
 )
 
-conversation.predict(input="Hello, nice to meet you!")
-
-conversation.predict(input="Tell me about the Einstein-Szilard Letter in 2 lines ")
-
-    
-print("\n\n## memory.buffer ##:",memory.buffer)
-
-print("\n\n## memory.load_memory_variables({}) ##:",memory.load_memory_variables({}))
-
-memory.save_context({"input": "Very Interesting."}, 
-                    {"output": "Yes, it was my pleasure as an AI to answer."})
-
-print("\n\n## memory.load_memory_variables({}) ##:",memory.load_memory_variables({}))
-
-
-conversation.memory
-
-## part 02, save the conversation memory
-pickled_str = pickle.dumps(conversation.memory)
-
-print("\n\n## pickled_str ##:", pickled_str)
-
-with open('memory.pkl','wb') as f:
-    f.write(pickled_str)
-
-# ## part 03, load the conversation memory
-# new_memory_load = open('memory.pkl','rb').read()
-
-# llm = ChatOpenAI(temperature=0.0)
-# reload_conversation = ConversationChain(
-#     llm=llm, 
-#     memory = pickle.loads(new_memory_load),
-#     verbose=True
-# )
-
-# print("\n\n## reload_conversation.memory.buffer ##:",reload_conversation.memory.buffer)
+print("\n\n## reload_conversation.memory.buffer ##:",reload_conversation.memory.buffer)
 
