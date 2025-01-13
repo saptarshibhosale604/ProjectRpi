8849eafe9b40e8c5fae52e33fce2dd7e9e104d6c
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-13 17:04:53 +0530

    Update agent.py

--- Rpi/ChatBot/Langchain/agent.py
+++ Rpi/ChatBot/Langchain/agent.py
@@ -5,8 +5,7 @@ from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 
-from langchain_community.tools import toolShell
-from langchain_community.tools import YouTubeSearchTool
+from langchain_community.tools import ShellTool, YouTubeSearchTool
 
 import os
 
