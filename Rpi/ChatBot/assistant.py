150928cf622a7cceb60468b2f3a0fa049b0357ca
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2025-01-15 13:49:27 +0530

    Update assistant.py

--- Rpi/ChatBot/assistant.py
+++ Rpi/ChatBot/assistant.py
@@ -10,6 +10,9 @@ print("Initialized assistant.py")
 
 conversationMode = "wakeUp" 	# sleep: Go to Hibernate
 						                  # wakeUp: Goint to answer the user input
+inputMode = "text" # text / speech
+outputMode = "text" # text / speech
+
 listWakeUpCalls = ["hey there", "hi there", "hey rpi"]
 listSleepCalls = ["sleep now", "go to sleep", "we are done", "got it"]
 
@@ -68,65 +71,125 @@ def InitializingLogging():
 
 InitializingLogging()
 
-def Main():
+def BasicCmds(userInput):
 	global conversationMode
+	global inputMode
+	global outputMode
 	
+	# Checking for input mode
+	elif (userInput.lower() == "input mode text"):
+		inputMode = "text"
+		return True
+	
+	elif (userInput.lower() == "input mode speech"):
+		inputMode = "speech"
+		return True
+
+	# Checking for output mode
+	elif (userInput.lower() == "output mode text"):
+		outputMode = "text"
+		return True
+	
+	elif (userInput.lower() == "output mode speech"):
+		outputMode = "speech"
+		return True
+
+	# Checking for wake up call
+	if any(call in userInput.lower() for call in listWakeUpCalls):
+		conversationMode = "wakeUp"
+		return True
+	
+	# Checking for sleep call
+	elif any(call in userInput.lower() for call in listSleepCalls):
+		conversationMode = "sleep"
+		return True
+
+	else:
+		print("## inputMode:", inputMode, ":outputMode:" , outputMode, ":conversationMode:" , conversationMode, "##")
+		return False
+
+def Input():	
+	global inputMode
+		
+	## ## Input ## ##
 	# Getting user input
 	# userInput = "Hey there how its going on?" # sample 
-	userInput = input("userInput: ")	# Text 
-	# userInput = STT.Main()			# Speech To Text
+	if(inputMode == "text"):
+		userInput = input("userInput: ")	# Text 
+	elif(inputMode == "speech"):
+		userInput = STT.Main()			# Speech To Text
+	else():
+		print("Error: Invalid inputMode:", inputMode)
 	
 	# ~ print("userInput:",userInput)	
 	logger.info(f"userInput: {userInput}")
+	return userInput
+	
+def Processing(userInput):
+	global conversationMode
+	
+	if(BasicCmds(userInput)):
+		return
+		
+	
+	# for debug only conversation mode only wake up
+	# ~ conversationMode = "wakeUp"
+	
+		
+	# ~ # Checking for sleep call
+	# ~ if any(call in userInput.lower() for call in listSleepCalls):
+		# ~ conversationMode = "sleep"
+		
+	# if(debug01): print("conversationMode:",conversationMode)
+	
+	if (conversationMode == "wakeUp"):
+		# userInputToScriptInvocation
+		
+		# ~ terminalOutput = UITSI.Main(userInput)
+		# ~ print(f"TerminalOutput: {terminalOutput}")
+		
+		# ~ if(terminalOutput is not None):
+			# ~ # LLM User Responce generation
+		# ~ else:
+			# ~ # LLM General Question
+		
+		
+		# define role to userInput
+		# userInput = roleDefining + userInput			
+		logger.info(f"userInputWithDefinedRole: {userInput}")
+		
+		# Getting responce from LLM model
+		# llmResponce = LLM.Main(userInput)
+		
+		global threadId
+		threadId += 1	
+		agentResponce = Agent.Main(userInput, threadId)
+
+		return agentResponce
+
+def Output(assistantOutput):	
+	global outputMode
+	
+	if(outputMode == "text"):
+		logger.info(f"llmResponce: {agentResponce}")	# Text 
+	elif(outputMode == "speech"):	
+		TTS.Main(llmResponce) 			# Text to speech
+	else():
+		print("Error: Invalid outputMode:", outputMode)
+			
+
+def Main():
+	
+	userInput = Input()
 	
 	if (userInput is not None):
-		if(debug01): print("userInput Not null")
-		
-		# for debug only conversation mode only wake up
-		# ~ conversationMode = "wakeUp"
-		
-		# Checking for wake up call
-		if any(call in userInput.lower() for call in listWakeUpCalls):
-			conversationMode = "wakeUp"
-		
-		# Checking for sleep call
-		elif any(call in userInput.lower() for call in listSleepCalls):
-			conversationMode = "sleep"
-			
-		# ~ # Checking for sleep call
-	   	# ~ if any(call in userInput.lower() for call in listSleepCalls):
-			# ~ conversationMode = "sleep"
-			
-		if(debug01): print("conversationMode:",conversationMode)
-		
-		if (conversationMode == "wakeUp"):
-			# userInputToScriptInvocation
-			
-			# ~ terminalOutput = UITSI.Main(userInput)
-			# ~ print(f"TerminalOutput: {terminalOutput}")
-			
-			# ~ if(terminalOutput is not None):
-				# ~ # LLM User Responce generation
-			# ~ else:
-				# ~ # LLM General Question
-			
-			
-			# define role to userInput
-			# userInput = roleDefining + userInput			
-			logger.info(f"userInputWithDefinedRole: {userInput}")
-			
-			# Getting responce from LLM model
-			# llmResponce = LLM.Main(userInput)
-			
-			global threadId
-			threadId += 1	
-			agentResponce = Agent.Main(userInput, threadId)
-			
-			logger.info(f"llmResponce: {agentResponce}")
-						
-			# ~ # Text to speech
-			# TTS.Main(llmResponce)
 
+		if(debug01): print("input Not null")
+		assistantOutput = Processing(userInput)
+
+		if (assistantOutput is not None):
+			Output(assistantOutput)
+		
 while(True):
 	Main()
 	
