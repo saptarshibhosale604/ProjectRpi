7b14ee9922f146cf414c35f68bcf1b0d21e03c80
root <root@ip-172-31-7-240.ap-south-1.compute.internal>
2025-03-01 03:34:46 +0000

    docker notification cron working

--- Docker/Notification/Cron/notify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Your custom notification action.
-# For example, appending a notification message to /var/log/notify.log
-
-echo "Notification: It is your time now. Your scheduled message goes here."
